82c821fc893422680674a41e1e2c23258ad8ce23
hkyinked <bunchblankspaces@gmail.com>
2020-12-16 17:53:25 -0500

    remove sleep

--- pacman_script.py
+++ pacman_script.py
@@ -9,16 +9,14 @@ top.title('Pacman Installation')
 top.geometry('200x140')
 init = Label(top, text="Starting application install").grid(row=0, columnspan=2, pady=10, padx=10)
 pip = ['sudo', 'pacman', '-S', 'python-pip']
-#user = Entry(top,)
 first_label = subprocess.Popen((pip),shell=False,stdout=subprocess.PIPE,text=True)
-first_label.wait(7)
-#time.sleep(4)
-print(first_label.stdout)
+print(first_label.returncode)
 #close button
 top.close_button = Button(top, text="Back", command=top.destroy, bg='grey', font='black')
 top.close_button.grid(pady=5,padx=10)
 
-if first_label == 0:
+if first_label:
+    print(first_label.returncode)
     WAI = Label(top, text="Application installed sucessfully")
     WAI.grid(row=5, columnspan=1,padx=10, pady=10)
 #def script():
